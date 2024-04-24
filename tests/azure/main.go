ab9c52a0833f1cc71e437e520d5324e94a597fb1
JeganThavamurugan <jegan@tailwinds.ai>
2024-04-24 15:10:43 +0530

    changes

---
+++ tests/azure/main.go
@@ -0,0 +1,53 @@
+// Copyright 2018 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+	//"os/exec"
+	"github.com/GoogleCloudPlatform/terraformer/cmd"
+	//aws_terraforming "github.com/GoogleCloudPlatform/terraformer/providers/aws"
+)
+
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+	log.SetOutput(os.Stdout)
+
+	tCommand := cmd.NewCmdRoot()
+	pathPattern := "{output}/{provider}/"
+	tCommand.SetArgs([]string{
+		"import",
+		"azure",
+		//"--location=us-east-1",
+		//"--resources=ec2_instance",
+		"-r resource_group",
+		//"-r virtual_machine",
+		//"--filter=Name=tags.owner;Value=jegan",
+		//"--filter=resource_group=/subscriptions/64d22802-2f81-4581-8dcc-865126631a8c/resourceGroups/Jegan-Dev_group",
+		//"--filter=ec2=i-04a3058688e50ee7c",
+		"--verbose",
+		"--compact",
+		"--path-pattern=" + pathPattern,
+	})
+	start := time.Now()
+	if err := tCommand.Execute(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	log.Printf("Importing took %s", time.Since(start))
+}
+
